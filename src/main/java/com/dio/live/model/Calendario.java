d23274147979ba10ad581b17b98a1ca8264b5d11
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-08-03 07:08:13 -0300

    Create Calendario.java

---
+++ src/main/java/com/dio/live/model/Calendario.java
@@ -0,0 +1,27 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+
+public class Calendario {
+    @Id
+    private long id;
+    @ManyToOne
+    private TipoData tipoData;
+    private String descricao;
+    private LocalDateTime dataEspecial;
+
+}

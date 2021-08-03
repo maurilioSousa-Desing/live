27584efbb66c82aada3f8e255722b97360e35c16
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-08-03 07:08:03 -0300

    Create BancoHoras.java

---
+++ src/main/java/com/dio/live/model/BancoHoras.java
@@ -0,0 +1,35 @@
+package com.dio.live.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+
+public class BancoHoras {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class BancoHorasId implements Serializable {
+        private long idBancoHoras;
+        private long idMovimentacao;
+        private long idUsuario;
+    }
+    @EmbeddedId
+    private BancoHorasId id;
+    private LocalDateTime dataTrabalhada;
+    private BigDecimal quantHorasTrabalhada;
+    private BigDecimal saldoHoras;
+}
